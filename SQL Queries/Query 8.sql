d8d5bb6d14cf9a03aa6a0c35d180f073d7b2b5d9
SIVASUNDAR T P <121023707+Sivasundar3@users.noreply.github.com>
2025-09-12 10:43:21 +0530

    Update Query 8.sql

--- SQL Queries/Query 8.sql
+++ SQL Queries/Query 8.sql
@@ -1,3 +1,15 @@
+/* 8.Create a report that includes the quarter, total comments, and total saves recorded for each post category. Assign the following quarter groupings:
+                         (January, February, March) → “Q1”
+                         (April, May, June) → “Q2”
+                         (July, August, September) → “Q3”
+
+The final output columns should consist of:
+                     • post_category
+                     • quarter
+                     • total_comments
+                     • total_saves */
+
+
 select fc.post_category,
        case when d.month_name in ('January' ,'February' , 'March') then 'Q1'
             when d.month_name in ('April' ,'May' , 'June') then 'Q2'
