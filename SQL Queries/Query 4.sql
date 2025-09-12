c9f01bd871a0ac8714513354ccb1e200b67878cc
SIVASUNDAR T P <121023707+Sivasundar3@users.noreply.github.com>
2025-09-12 10:36:48 +0530

    Update Query 4.sql

--- SQL Queries/Query 4.sql
+++ SQL Queries/Query 4.sql
@@ -1,3 +1,9 @@
+/* 4. Create a report to get the statistics for the account. The final output includes the following fields:
+                                       • month_name
+                                       • total_profile_visits
+                                       • total_new_followers */
+
+
 select d.month_name,
 	   sum(fa.profile_visits) as total_profile_visits,
        sum(fa.new_followers) as total_new_followers
